84c5ad5d9c16e86249576f3fc193a55fec783690
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2025-03-03 21:18:57 +0100

    Add gradle project

---
+++ plugin/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    }    
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(libs.jetbrains.annotations)
+    implementation(libs.spigot.api)
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+project.base.archivesName.set(rootProject.name)
+group = "fr.flowsqy.xpbarrel"
+version = "1.0.0-SNAPSHOT"
+
+tasks.processResources {
+    expand(Pair("version", version))
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
