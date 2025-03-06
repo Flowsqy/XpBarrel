9c6bcdda975109331b357ab0d2779a1d71778e1a
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2025-03-06 23:12:47 +0100

    Deserialize the old config

--- plugin/src/main/java/fr/flowsqy/xpbarrel/config/BarrelStorage.java
+++ plugin/src/main/java/fr/flowsqy/xpbarrel/config/BarrelStorage.java
@@ -1,10 +1,6 @@
 package fr.flowsqy.xpbarrel.config;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -20,33 +16,27 @@ import fr.flowsqy.xpbarrel.barrel.XpBarrelSnapshot;
 
 public class BarrelStorage {
 
-    private YamlConfiguration oldConfiguration = null;
-    private List<WorldConfiguration> configurations;
+    private final OldBarrelStorageLoader oldBarrelStorageLoader;
+    private final List<WorldConfiguration> configurations;
+
+    public BarrelStorage() {
+        oldBarrelStorageLoader = new OldBarrelStorageLoader();
+        configurations = new LinkedList<>();
+    }
 
     private record WorldConfiguration(@NotNull String name, @NotNull YamlConfiguration configuration) {
     }
 
     public void load(@NotNull File dataFolder, @NotNull Logger logger) {
-        final var oldStorage = new File(dataFolder, "storages.yml");
-        if (oldStorage.exists() && oldStorage.isFile()) {
-            oldConfiguration = YamlConfiguration.loadConfiguration(oldStorage);
-            try {
-                Files.move(oldStorage.toPath(), Path.of(dataFolder.getAbsolutePath(), "storages.yml.OLD"));
-            } catch (IOException e) {
-                logger.log(Level.SEVERE, "Could not move the old configuration", e);
-            }
-        }
+        oldBarrelStorageLoader.load(dataFolder, logger);
         final var storageDir = new File(dataFolder, "storage");
         if (!storageDir.exists()) {
-            configurations = Collections.emptyList();
             return;
         }
         if (!storageDir.isDirectory()) {
             logger.log(Level.SEVERE, storageDir.getAbsolutePath() + " is not a directory");
-            configurations = Collections.emptyList();
             return;
         }
-        configurations = new LinkedList<>();
         for (var storageFile : storageDir.listFiles()) {
             final var fileName = storageFile.getName();
             if (!fileName.endsWith(".yml")) {
@@ -60,14 +50,8 @@ public class BarrelStorage {
     @NotNull
     public Map<String, Map<BlockPosition, XpBarrelSnapshot>> loadBarrels(@NotNull Logger logger) {
         final Map<String, Map<BlockPosition, XpBarrelSnapshot>> loadedBarrels = new HashMap<>();
-        fillPreviousValues(loadedBarrels);
+        oldBarrelStorageLoader.fillPreviousValues(loadedBarrels);
         return loadedBarrels;
-    }
-
-    @NotNull
-    private void fillPreviousValues(@NotNull Map<String, Map<BlockPosition, XpBarrelSnapshot>> loadedBarrels) {
-        final var sectionList = oldConfiguration.getList("Instances");
-        System.out.println(sectionList);
     }
 
 }
---
+++ plugin/src/main/java/fr/flowsqy/xpbarrel/config/OldBarrelStorageLoader.java
@@ -0,0 +1,112 @@
+package fr.flowsqy.xpbarrel.config;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.jetbrains.annotations.NotNull;
+import org.yaml.snakeyaml.Yaml;
+
+import fr.flowsqy.xpbarrel.barrel.BlockPosition;
+import fr.flowsqy.xpbarrel.barrel.XpBarrelSnapshot;
+
+public class OldBarrelStorageLoader {
+
+    private Object yamlObject;
+
+    public void load(@NotNull File dataFolder, @NotNull Logger logger) {
+        final var oldStorage = new File(dataFolder, "storages.yml");
+        if (!oldStorage.exists() || !oldStorage.isFile()) {
+            return;
+        }
+        final var yaml = new Yaml();
+        final InputStream inputStream;
+        try {
+            inputStream = new FileInputStream(oldStorage);
+        } catch (FileNotFoundException e) {
+            logger.log(Level.SEVERE, "Couldn't find the old configuration", e);
+            return;
+        }
+        yamlObject = yaml.load(inputStream);
+        boolean a = true;
+        if (a) {
+            return;
+        }
+        try {
+            Files.move(oldStorage.toPath(), Path.of(dataFolder.getAbsolutePath(), "storages.yml.OLD"));
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Could not move the old configuration", e);
+        }
+    }
+
+    @NotNull
+    public void fillPreviousValues(@NotNull Map<String, Map<BlockPosition, XpBarrelSnapshot>> loadedBarrels) {
+        if (!(yamlObject instanceof Map rootMap)) {
+            return;
+        }
+        final var rawInstances = rootMap.get("Instances");
+        if (!(rawInstances instanceof List instancesList)) {
+            return;
+        }
+        for (var rawEntry : instancesList) {
+            if (!(rawEntry instanceof Map entryMap)) {
+                continue;
+            }
+            final var rawType = entryMap.get("Type");
+            if (!(rawType instanceof String type)) {
+                continue;
+            }
+            if (!type.equals("XP_BARREL")) {
+                continue;
+            }
+            final var rawOwner = entryMap.get("Owner");
+            if (!(rawOwner instanceof String owner)) {
+                continue;
+            }
+            final UUID ownerId;
+            try {
+                ownerId = UUID.fromString(owner);
+            } catch (IllegalArgumentException e) {
+                continue;
+            }
+            final var rawLocation = entryMap.get("Location");
+            if (!(rawLocation instanceof Map locationMap)) {
+                continue;
+            }
+            final var rawWorld = locationMap.get("world");
+            if (!(rawWorld instanceof String world)) {
+                continue;
+            }
+            final var rawX = locationMap.get("x");
+            if (!(rawX instanceof Number x)) {
+                continue;
+            }
+            final var rawY = locationMap.get("y");
+            if (!(rawY instanceof Number y)) {
+                continue;
+            }
+            final var rawZ = locationMap.get("z");
+            if (!(rawZ instanceof Number z)) {
+                continue;
+            }
+            int storedLevel = 0;
+            if (entryMap.get("StoredExperience") instanceof Number storedExperience) {
+                storedLevel = storedExperience.intValue();
+            }
+            final var blockPosition = new BlockPosition(x.intValue(), y.intValue(), z.intValue());
+            final var loadedBarrelsInWorld = loadedBarrels.computeIfAbsent(world, k -> new HashMap<>());
+            loadedBarrelsInWorld.put(blockPosition, new XpBarrelSnapshot(storedLevel, ownerId));
+        }
+    }
+
+}
