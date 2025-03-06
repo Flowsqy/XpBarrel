6ed0f1fe9b067d1073f77c1fc6ebe6a97f44d828
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2025-03-06 20:07:22 +0100

    Implement InteractListener

---
+++ plugin/src/main/java/fr/flowsqy/xpbarrel/listener/InteractListener.java
@@ -0,0 +1,39 @@
+package fr.flowsqy.xpbarrel.listener;
+
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.jetbrains.annotations.NotNull;
+
+import fr.flowsqy.xpbarrel.barrel.BarrelManager;
+import fr.flowsqy.xpbarrel.barrel.BlockPosition;
+
+public class InteractListener implements Listener {
+
+    private final BarrelManager barrelManager;
+
+    public InteractListener(@NotNull BarrelManager barrelManager) {
+        this.barrelManager = barrelManager;
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onInteract(PlayerInteractEvent event) {
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+            return;
+        }
+        final var interactedBlock = event.getClickedBlock();
+        if (interactedBlock.getType() != Material.BARREL) {
+            return;
+        }
+        final var position = BlockPosition.from(interactedBlock.getLocation());
+        final var xpBarrel = barrelManager.getBarrelAt(interactedBlock.getWorld().getName(), position);
+        if (xpBarrel == null) {
+            return;
+        }
+        event.setCancelled(true);
+    }
+
+}
