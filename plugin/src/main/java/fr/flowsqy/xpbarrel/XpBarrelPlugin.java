5a85223ab382631737461dd56ffd1a5e6ed91f11
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2025-03-06 20:11:39 +0100

    Register listeners

--- plugin/src/main/java/fr/flowsqy/xpbarrel/XpBarrelPlugin.java
+++ plugin/src/main/java/fr/flowsqy/xpbarrel/XpBarrelPlugin.java
@@ -2,10 +2,22 @@ package fr.flowsqy.xpbarrel;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import fr.flowsqy.xpbarrel.barrel.BarrelManager;
+import fr.flowsqy.xpbarrel.listener.BreakListener;
+import fr.flowsqy.xpbarrel.listener.InteractListener;
+import fr.flowsqy.xpbarrel.listener.ProtectListener;
+
 public class XpBarrelPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
+        // TODO Init barrel manager
+        final var barrelManager = new BarrelManager(null);
+        final var pluginManager = getServer().getPluginManager();
+        pluginManager.registerEvents(new BreakListener(barrelManager), this);
+        pluginManager.registerEvents(new ProtectListener(barrelManager), this);
+        pluginManager.registerEvents(new InteractListener(barrelManager), this);
     }
 
 }
