c7e288414f0fd170aeae78441220ae7fde04feb5
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2025-03-06 23:29:59 +0100

    Deserialize current storage

--- plugin/src/main/java/fr/flowsqy/xpbarrel/config/BarrelStorage.java
+++ plugin/src/main/java/fr/flowsqy/xpbarrel/config/BarrelStorage.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -51,6 +52,35 @@ public class BarrelStorage {
     public Map<String, Map<BlockPosition, XpBarrelSnapshot>> loadBarrels(@NotNull Logger logger) {
         final Map<String, Map<BlockPosition, XpBarrelSnapshot>> loadedBarrels = new HashMap<>();
         oldBarrelStorageLoader.fillPreviousValues(loadedBarrels);
+        for (var worldConfig : configurations) {
+            final var loadedBarrelsInWorld = loadedBarrels.computeIfAbsent(worldConfig.name(), k -> new HashMap<>());
+            for (var key : worldConfig.configuration().getKeys(false)) {
+                final var barrelSection = worldConfig.configuration().getConfigurationSection(key);
+                if (barrelSection == null) {
+                    continue;
+                }
+                final var rawOwner = barrelSection.getString("owner");
+                final UUID owner;
+                try {
+                    owner = UUID.fromString(rawOwner);
+                } catch (IllegalArgumentException e) {
+                    logger.log(Level.WARNING, "Couldn't retrieve owner id", e);
+                    continue;
+                }
+                final var positionSection = barrelSection.getConfigurationSection("position");
+                if (positionSection == null) {
+                    logger.warning("Couldn't retrieve the position in " + barrelSection.getCurrentPath() + " ("
+                            + worldConfig.name() + ")");
+                    continue;
+                }
+                final int x = positionSection.getInt("x", 0);
+                final int y = positionSection.getInt("y", 0);
+                final int z = positionSection.getInt("z", 0);
+                final var position = new BlockPosition(x, y, z);
+                final int experience = barrelSection.getInt("experience", 0);
+                loadedBarrelsInWorld.put(position, new XpBarrelSnapshot(experience, owner));
+            }
+        }
         return loadedBarrels;
     }
 
