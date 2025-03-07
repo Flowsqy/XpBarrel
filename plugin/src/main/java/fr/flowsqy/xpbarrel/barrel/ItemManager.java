f451a4625c8061cd4d90263cacff8ca455efa90f
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2025-03-07 14:41:53 +0100

    Implement ItemManager#extractData

--- plugin/src/main/java/fr/flowsqy/xpbarrel/barrel/ItemManager.java
+++ plugin/src/main/java/fr/flowsqy/xpbarrel/barrel/ItemManager.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -49,8 +50,33 @@ public class ItemManager {
 
     @Nullable
     public ExtractDataResult extractData(@NotNull ItemStack itemStack) {
+        final var itemMeta = itemStack.getItemMeta();
+        final var itemPdc = itemMeta.getPersistentDataContainer();
+        final var barrelPdc = itemPdc.get(barrelKey, PersistentDataType.TAG_CONTAINER);
+        if (barrelPdc == null) {
+            return extractOldData(itemStack, itemPdc);
+        }
+        final var owner = barrelPdc.get(ownerKey, CustomPersistentDataType.UUID);
+        final int experience = barrelPdc.getOrDefault(experienceKey, PersistentDataType.INTEGER, 0);
+        return new ExtractDataResult(owner, experience);
+    }
 
-        return null;
+    @Nullable
+    public ExtractDataResult extractOldData(@NotNull ItemStack itemStack, @NotNull PersistentDataContainer itemPdc) {
+        final var type = itemPdc.get(oldTypeKey, PersistentDataType.STRING);
+        if (type == null || !type.equals("xp-barrel")) {
+            return null;
+        }
+        final int experience = itemPdc.getOrDefault(oldExperienceKey, PersistentDataType.INTEGER, 0);
+        final var rawOwner = itemPdc.get(oldOwnerKey, PersistentDataType.STRING);
+        UUID owner = null;
+        if (rawOwner != null) {
+            try {
+                owner = UUID.fromString(rawOwner);
+            } catch (IllegalArgumentException ignored) {
+            }
+        }
+        return new ExtractDataResult(owner, experience);
     }
 
 }
