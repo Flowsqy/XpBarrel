daa4011e0fc0127ffa1b2ae9c3a54e2b4f7ce0b2
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2025-03-11 17:29:44 +0100

    Protect barrel from entity explosion

--- plugin/src/main/java/fr/flowsqy/xpbarrel/listener/ProtectListener.java
+++ plugin/src/main/java/fr/flowsqy/xpbarrel/listener/ProtectListener.java
@@ -1,11 +1,13 @@
 package fr.flowsqy.xpbarrel.listener;
 
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBurnEvent;
 import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.jetbrains.annotations.NotNull;
@@ -21,35 +23,33 @@ public class ProtectListener implements Listener {
         this.barrelManager = barrelManager;
     }
 
-    @EventHandler(priority = EventPriority.NORMAL)
-    public void onExplode(BlockExplodeEvent event) {
-        final var blockState = event.getExplodedBlockState();
-        if (blockState.getType() != Material.BARREL) {
-            return;
-        }
-        final var position = BlockPosition.from(blockState.getLocation());
-        final var xpBarrel = barrelManager.getBarrelAt(blockState.getWorld().getName(), position);
-        if (xpBarrel == null) {
-            return;
-        }
-        event.setCancelled(true);
-    }
-
-    @EventHandler(priority = EventPriority.NORMAL)
-    public void onBurn(BlockBurnEvent event) {
-        final var block = event.getBlock();
+    private boolean isBarrel(@NotNull Block block) {
         if (block.getType() != Material.BARREL) {
-            return;
+            return false;
         }
         final var position = BlockPosition.from(block.getLocation());
         final var xpBarrel = barrelManager.getBarrelAt(block.getWorld().getName(), position);
-        if (xpBarrel == null) {
-            return;
-        }
-        event.setCancelled(true);
+        return xpBarrel != null;
     }
 
-    @EventHandler(priority = EventPriority.NORMAL)
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onEntityExplode(EntityExplodeEvent event) {
+        event.blockList().removeIf(this::isBarrel);
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onBlockExplode(BlockExplodeEvent event) {
+        event.blockList().removeIf(this::isBarrel);
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    public void onBurn(BlockBurnEvent event) {
+        if (isBarrel(event.getBlock())) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     public void onItemTransfer(InventoryMoveItemEvent event) {
         final var destinationInventory = event.getDestination();
         if (destinationInventory.getType() != InventoryType.BARREL) {
